222d0ddaecb6a5ae9757c3477053f41176c9bd1b
Lisa DeBona <annalisa.quiamco@gmail.com>
2022-05-17 01:16:00 -0400

    Built gallery page

--- page-gallery.php
+++ page-gallery.php
@@ -27,18 +27,27 @@ get_header(); ?>
             $caption = get_sub_field('caption');
             $gallery_text = ($caption) ? ' data-caption="'.$caption.'"':'';
             $video_thumbnail = '';
+            $youtubeId = '';
+            $vimeoId = '';
             if($type=='video' && $video) {
               if (str_contains($video, 'youtu.be')) {
                 $youtubeId = basename($video);
-              } else {
+              } 
+              else if (str_contains($video, 'youtube.com')) {
                 $parts = parse_url($video);
                 parse_str($parts['query'], $query);
                 $youtubeId = (isset($query['v']) && $query['v']) ? $query['v']:'';
               }
 
               if($youtubeId) {
-                $video_thumbnail = 'http://i3.ytimg.com/vi/'.$youtubeId.'/hqdefault.jpg';
+                $video_thumbnail = 'http://i3.ytimg.com/vi/'.$youtubeId.'/maxresdefault.jpg';
               }
+              
+              if (str_contains($video, 'vimeo.com')) {
+                $vimeoId = basename($video);
+                $hash = unserialize(file_get_contents("https://vimeo.com/api/v2/video/$vimeoId.php"));
+                $video_thumbnail = ( isset($hash[0]['thumbnail_large']) && $hash[0]['thumbnail_large'] ) ? $hash[0]['thumbnail_large'] : '';
+              } 
             }
             if($type=='image' && $image ) { ?>
             <figure class="type_image">
@@ -48,9 +57,13 @@ get_header(); ?>
               </a>
             </figure>
             <?php } else if($type=='video' && $video ) { ?>
-            <figure class="type_video">
+            <figure class="type_video <?php echo ($youtubeId) ? 'youtube':'vimeo' ?>">
               <a data-fancybox="gallery" data-src="<?php echo $video ?>"<?php echo $gallery_text ?>>
-                <span class="image" style="background-image:url('<?php echo $video_thumbnail ?>')"><i class="arrow"></i></span>
+                <?php if ($video_thumbnail) { ?>
+                  <span class="image" style="background-image:url('<?php echo $video_thumbnail ?>')"><i class="arrow"></i></span>
+                <?php } else { ?>
+                  <span class="image noThumbnail"><i class="arrow"></i></span>
+                <?php } ?>
                 <img src="<?php echo THEMEURI ?>images/rectangle-lg.png" alt="" aria-hidden="true" />
               </a>
             </figure>
